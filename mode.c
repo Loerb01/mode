4d7df315c96a5db0071da218cbfec3e9c3c298d7
Leo Brown <leo.brown.lcrb@gmail.com>
2024-12-15 06:29:48 -0500

    Slightly reworked control flow and added begininngs of status bar

--- mode.c
+++ mode.c
@@ -4,7 +4,13 @@
 #include <stdlib.h>
 #include "mode_file.h"
 
+struct cursLoc {
+	unsigned char x;
+	unsigned char y;
+};
+
 void scrollScrn(mode_Line* topLine);
+void printStatus(const char* modeName, struct cursLoc cursLoc);
 
 int main(int argc, char** argv) {
 	//Verify argument~s~
@@ -22,9 +28,12 @@ int main(int argc, char** argv) {
 	//initialize ncurses
 	setlocale(LC_ALL, "");
 	initscr();
+	start_color();
 	cbreak();
 	noecho();
 	keypad(stdscr, TRUE);
+
+	init_pair(1, 0, 2);
 
 	//Get term cells size
 	int max_x = 0;
@@ -32,6 +41,7 @@ int main(int argc, char** argv) {
 	getmaxyx(stdscr, max_y, max_x);
 
 	//Variables for prog to use
+	bool run = true;
 	char gottenCh = 0;
 	int topLineNum = 0;
 	mode_Line* topLine;
@@ -39,10 +49,7 @@ int main(int argc, char** argv) {
 	mode_Line* workingLine;
 
 	//Desired cursos location
-	struct cursLoc {
-		unsigned char x;
-		unsigned char y;
-	}cursLoc;
+	struct cursLoc cursLoc;
 	cursLoc.x = 0;
 	cursLoc.y = 0;
 
@@ -53,9 +60,15 @@ int main(int argc, char** argv) {
 	//Print initial file contents.
 	scrollScrn(topLine);
 	move(0, 0);
+	printStatus("Mode", cursLoc);
+	move(0, 0);
 
-	while(gottenCh != 'q') {
+	while(run) {
+		gottenCh = getch();
 		switch(gottenCh) {
+			case 'q':
+				run = false;
+				break;
 			case 'n':
 				if(max_y - cursLoc.y < 6 && topLine->nextLine != NULL) {
 					topLine = topLine->nextLine;
@@ -101,11 +114,16 @@ int main(int argc, char** argv) {
 					cursLoc.x--;
 				}
 				break;
-		}
 
+			default:
+				break;
+		}
+	
+		printStatus("Mode", cursLoc);
+
+		//Don't touch below this point, ensures accuracy of cursor location
 		move(cursLoc.y, cursLoc.x);
 		refresh();
-		gottenCh = getch();
 	}
 
 	mode_closeFile(openFile);
@@ -128,6 +146,24 @@ void scrollScrn(mode_Line* topLine) { //Redefine for arb window, and macroize?
 		workingLine = workingLine->nextLine;
 	}
 	move(0, 0);
-	return;
+}
+
+void printStatus(const char* modeName, struct cursLoc cursLoc) {
+	int max_x = 0;
+	int max_y = 0;
+	getmaxyx(stdscr, max_y, max_x);
+
+	char* toPrint = malloc(max_x);
+	if(!toPrint) {
+		exit(99);
+	}
+
+	int used = snprintf(toPrint, max_x, "%s (%d,%d):", modeName, cursLoc.x, cursLoc.y);
+	attr_set(A_NORMAL, 1, NULL);
+	mvaddnstr(max_y-1, 0, toPrint, max_x);
+	for(int i = used; i < max_x; i++) {
+		addch(' ');
+	}
+	attr_set(A_NORMAL, 0, NULL);
 }
 
